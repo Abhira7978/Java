69ad18a58f7c3bafd27b42c6283755827e4c35fd
Abhira <abhijeetsahu7978@gmail.com>
2025-10-14 00:01:04 +0530

    Identify If The Person Is Adult or Not

---
+++ if else/Age.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Age {
+    public static void main(String a[]){
+        System.out.println("Enter the age:");
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+
+        if(age>18){
+            System.out.println("Adult");
+        }else{
+            System.out.println("not Adult");
+        }
+    }
+    
+}
