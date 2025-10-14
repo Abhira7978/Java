ab74193aa26ad5c77d5d9d2484ddafd359103c1e
Abhira <abhijeetsahu7978@gmail.com>
2025-10-14 23:11:32 +0530

    while loop

--- loop/forloop.java
+++ loop/forloop.java
@@ -5,8 +5,8 @@ public class forloop {
     public static void main(String[] args) {
         
     
-    for(int x =0;x<100;x++){
-        System.out.println("Hello");
+    for(int x =0;x<10;x++){
+        System.out.println(x);
     }
 }
     
