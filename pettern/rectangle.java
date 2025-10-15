db425938cf080692bcfeb69c8b9496709b1668d6
Abhira <abhijeetsahu7978@gmail.com>
2025-10-15 23:40:59 +0530

    It  Is A Code Of Solid Rectangle

---
+++ pettern/rectangle.java
@@ -0,0 +1,23 @@
+package pettern;
+
+import java.util.Scanner;
+
+public class rectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+    
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                System.out.print("* ");
+            }
+            System.out.println(" \n");
+            
+
+
+        }
+        
+    }
+    
+}
