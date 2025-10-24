c4676799703882c55de2e8b09a95bf2f6fcaa8f3
Abhira <abhijeetsahu7978@gmail.com>
2025-10-24 23:18:35 +0530

    It Is To Add Two Numbers

---
+++ Function/addition.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class addition {
+    public static int calculateSum(int a,int b) {
+        int sum = a+b;
+        return sum;
+    }
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The First Number:");
+        int a = sc.nextInt();
+        System.out.println("Enter The Second number :");
+        int b=sc.nextInt();
+
+        int sum = calculateSum(a, b);
+        System.out.println("Sum Of Two number ="+sum);
+
+    }
+    
+}
