a93f3ab10d0794aee9e38d0cd54b73312bbc090c
Abhira <abhijeetsahu7978@gmail.com>
2025-11-08 01:19:28 +0530

    It Is To Add the Substring

--- Strings/Functions.java
+++ Strings/Functions.java
@@ -4,20 +4,38 @@ import java.util.Scanner;
 
 public class Functions {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter The First name");
-        String Firstname = sc.next();
-        System.out.println("Enter The Last name");
-        String Lastname =sc.next();
-        String Fullname = Firstname+" "+Lastname;
-        System.out.println(Fullname);
-        System.out.println(Fullname.length());
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter The First name");
+        // String Firstname = sc.next();
+        // System.out.println("Enter The Last name");
+        // String Lastname =sc.next();
+        // String Fullname = Firstname+" "+Lastname; // It Is To concatinate the strings
+        // System.out.println(Fullname);
+        // System.out.println(Fullname.length());//It is To find The Number Of Characters Pass In A Strings
 
 
-        for(int i = 0;i<=Fullname.length();i++){
-            System.out.print(Fullname.charAt(i));
-        }
+
+
+        // for(int i = 0;i<=Fullname.length();i++){
+        //     System.out.print(Fullname.charAt(i)); //It Is To return All The Index Of The String
+        // }
+
+        
+Scanner sc = new Scanner(System.in);
+String Sentence = sc.nextLine();
+
+String name = Sentence.substring(4);
+System.out.println(name);
+
+
 
     }
     
 }
+
+
+
+
+
+
+
