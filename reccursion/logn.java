2b000f7192ac6a4990f213b4992f564f01acd7c6
Abhira <abhijeetsahu7978@gmail.com>
2025-12-10 22:11:46 +0530

    It Is To Find the Power in LOG n stack

---
+++ reccursion/logn.java
@@ -0,0 +1,28 @@
+package reccursion;
+
+import java.util.Scanner;
+
+public class logn {
+    public static int calcpower(int x,int n){
+        if(n==0){
+            return 1;
+        }
+        if(x==0){
+            return 1;
+        }
+        if(n%2==0){
+            return calcpower(x, n/2)*calcpower(x, n/2);
+        }else{
+            return calcpower(x, n/2)*calcpower(x, n/2)*x;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int  x =  sc.nextInt();
+        int n = sc.nextInt();
+       int ans= calcpower(x, n);
+       System.out.println(ans);
+        
+    }
+    
+}
