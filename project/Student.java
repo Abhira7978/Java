c09139bc9e1e591525b629090f44a69cfae4836e
Abhira <abhijeetsahu7978@gmail.com>
2025-12-17 00:20:22 +0530

    It Is To Count The Path In Matrix

---
+++ project/Student.java
@@ -0,0 +1,10 @@
+package project;
+
+import java.util.Scanner;
+
+public class Student {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter The Name Of The Student:");
+    
+    
+}
---
+++ reccursion/countpath.java
@@ -0,0 +1,22 @@
+public class countpath {
+    public static int countPath(int i,int j,int n,int m){
+        if(i==n||j==m){
+            return 0;
+        }
+        if(i==n-1&&j==m-1){
+            return 1;
+        }
+        int downpath=countPath(i+1, j, n, m);
+        int rightpath=countPath(i, j+1, n, m);
+        return downpath+rightpath;
+
+
+    }
+    public static void main(String[] args) {
+        int n=3, m =4;
+        int totalpath=countPath(0, 0, n, m);
+        System.out.println(totalpath);
+        
+    }
+    
+}
