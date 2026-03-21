82c619e0b71bce09f12b5b5a1e038af9e103e9b4
Abhira <abhijeetsahu7978@gmail.com>
2026-03-21 21:14:44 +0530

    It Is to Print Both 1 To 5 And 5 to 1

--- reccursion/printnumber.java
+++ reccursion/printnumber.java
@@ -1,19 +1,48 @@
 
 
+// public class printnumber {
+//     public static void printNumb(int n) {
+//         if (n==0) {
+//             return;
+//         }
+//         // for(int i =5;i>0;i--){
+//         //     System.out.println(i);
+//         // }
+//         System.out.println(n);
+//         printNumb(n-1);
+        
+//     }
+//     public static void main(String[] args) {
+//         int n = 5;
+//         printNumb(n);
+        
+//     }
+    
+    
+// }
+
+
+
+
+
+
+
+
+// It is To Print The Number 1 To 5
 public class printnumber {
     public static void printNumb(int n) {
-        if (n==0) {
+        if (n==6) {
             return;
         }
         // for(int i =5;i>0;i--){
         //     System.out.println(i);
         // }
         System.out.println(n);
-        printNumb(n-1);
+        printNumb(n+1);
         
     }
     public static void main(String[] args) {
-        int n = 5;
+        int n = 1;
         printNumb(n);
         
     }
